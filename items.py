b7c4b94795931da9b02f351a3475949d206ec3d6
rariera <rariera02@gmail.com>
2017-06-20 13:45:49 +1000

    Added the extra files for making it work!

---
+++ items.py
@@ -0,0 +1,115 @@
+import tools
+
+#This will be the info about all the objects in the game.
+coconut = {
+    'name': 'coconut',
+    'desc': "Here is a coconut",
+    'getable': True,
+    'edible': True,
+    'usable': False,
+    'health': 5,
+    'inv': 0
+        }
+rope = {
+    'name': 'rope',
+    'desc': 'Here is a wet piece of rope. It must have washed up from your shipwreck.',
+    'getable': True,
+    'edible': False,
+    'usable': True,
+    'health': 0,
+    'inv': 0
+    }
+seagull = {
+    'name': 'seagull',
+    'desc': '''A seagull swoops in the sky, searching for food. It eyes you in a way that
+makes you feel uncomfortable...''',
+    'getable': False,
+    'edible': True,
+    'usable': False,
+    'health': 8,
+    'inv': 0
+    }
+rock = {
+    'name': 'rock',
+    'desc': 'Here is a sharp piece of rock. Maybe you could use it to make something...',
+    'getable': True,
+    'edible': False,
+    'usable': True,
+    'health': 0,
+    'inv': 0
+    }
+starfish = {
+    'name': 'starfish',
+    'desc': 'The starfish contemplates life in a rockpool...',
+    'getable': True,
+    'edible': True,
+    'usable': True,
+    'health': 6,
+    'inv': 0
+    }
+shellfish = {
+    'name': 'shellfish',
+    'desc': 'A small shellfish sticks to this rocky outcrop. Looks delicious...',
+    'getable': True,
+    'edible': True,
+    'usable': False,
+    'health': 3,
+    'inv': 0
+    }
+stick = {
+    'name': 'stick',
+    'desc': '''An old stick. You stare at it for a moment. You wonder about what it could have been, and
+    what it may yet become.''',
+    'getable': True,
+    'edible': False,
+    'usable': False,
+    'health': 0,
+    'inv': 0
+    }
+vine = {
+    'name': 'vine',
+    'desc': 'You pull on a vine. It comes off in your hand. Easy to break, but may still be useful...',
+    'getable': True,
+    'edible': False,
+    'usable': False,
+    'health': 0,
+    'inv': 0
+    }
+berry = {
+    'name': 'berry',
+    'desc': """A purple berry. You roll it in your hand. It could be delicious, but what
+    if it kills you instead?""",
+    'getable': True,
+    'edible': True,
+    'usable': False,
+    'health': 0,
+    'inv': 0
+    }
+rock = {
+	'name' : 'rock',
+	'desc': '''Before you lies a rock. You weigh it in your hand. Somewhere,
+	a memory stirs inside you. This is a sedimentary rock, the kind known as
+	'sandstone'....''',
+	'getable': True,
+	'edible': False,
+	'usable': False,
+	'health': 0,
+	'inv': 0
+	}
+
+#This is the results of tool uses...
+wood = {
+    'name': 'wood',
+    'desc': 'A piece of firewood. Can be used with flint and steel to make a fire.',
+    'getable': False,
+    'edible': False,
+    'usable': False,
+    'health': 0,
+    'inv': 0
+    }
+
+
+
+
+
+itemslist = [coconut, rope, seagull, rock, starfish, shellfish, stick, vine, berry, wood, axe]
---
+++ rooms.py
@@ -0,0 +1,87 @@
+import items
+import tools
+
+#This is the info about the rooms...
+beach = {
+    'locname': 'beach',
+    'location': [1,2],
+    'setting': """Around you are small yellow particles littering the beach. Oh wait, it\'s just sand.
+    To the right is an impassable shelf of rock. In front of you is a small winding trail,
+    leading into the jungle. To your left is more beach. And behind you?
+    Behind you there is the endless ocean, the waves lapping at the shore, so reminisent
+    of the storm that caused you to wash up on this desolate beach. To your left, a coconut tree sags""",
+    'tools': [],
+    'items': [coconut, rope, seagull]
+    }
+
+rocks = {
+    'locname': 'rocks',
+    'location': [1,1],
+    'setting': '''Around you are many rocks: misshappen lumps of granite that long ago fell here in a
+meteor shower. It seems many animals have made their home here: starfish, shellfish, in one of the
+rock pools, you even see a tiny trout!''',
+    'tools': [],
+    'items': [rock, starfish, shellfish]
+    }
+
+jungle = {
+    'locname': 'jungle',
+    'location': [2,2],
+    'setting': '''Around you vines hang low over the game trail, threatening to wrap you in their deadly
+embrace. By the trail, there are many sticks that litter the ground, and a berry bush on your right.
+Luckily, there is a clearing here that you should be able to Enter, to take shelter for the night.
+the game trail continues to the east, west, and north.''',
+    'tools': [axe],
+    'items': [stick, vine, berry]
+    }
+
+clearing = {
+    'locname': 'clearing',
+    'location': [2.5,2],
+    'setting': '''A soft clearing is spread out before you. The various trees around you branch together
+like a low ceiling, sheltering you from the darkened skies. For the first time in a long while, you feel
+safe.
+This looks like a good place to make camp.''',
+    'tools': [],
+    'items': []
+    }
+
+mountain = {
+    'locname': 'mountain',
+    'location': [3,2],
+    'setting': '''You find yourself at the foot of an impassable mountain. Around you, rocks litter the
+    ground, threatening to trip you at every turn.  North of you, a cave leads downwards into darkness.
+    To your ease, a short dirt path leads to a waterfall, overflowing into a mountain spring.''',
+    'tools': [],
+    'items': [rock]
+    }
+
+village = {
+    'locname': 'village',
+    'location': [2,1],
+    'setting': '''Around you stands the ruins of an ancient village. The ground is dusty and dry beneath
+your feet. You can see by the workmanship on the houses that this was once a society, filled with life
+and purpose. However, it seems as though any life in this village has long since passed on. Now the
+village stands desolate, alone on this dusty hillside. It suddenly hits you that perhaps
+you won't make it home after all...
+As you look about the dusty village, you begin to notice that many of the homes around here are in ruins,
+worn away by the course of time. However, one house near the edge of the village seems mostly
+intact. Perhaps you could enter here and take refuge?''',
+    'tools': [],
+    'items': []
+    }
+
+waterfall = {
+    'locname': 'waterfall',
+    'location': [3,3],
+    'setting': '''Before you there is a waterfall, filled with crystal clear water rushing quickly into
+the mountain spring at your feet. As it hits the surface of the water, it sends up white froth in all
+directions. To your west, there is a short dirt path, leading back to the mountain. To the south lies
+a grassy hillside, covered with wildflowers.
+There are [items]''',
+    'tools': [],
+    'items': []
+    }
+
+
+roomslist = [beach, rocks, jungle, clearing, mountain, village, waterfall]
